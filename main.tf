f19854d16ba11be0674e00234efb34bcc92486ce
sangameshragate <sanguragate@gmail.com>
2026-01-28 16:23:34 +0530

    Add AWS VPC and related resources in main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
+resource "aws_vpc" "vpc1" {
+  cidr_block = var.vpc_cidr
+  assign_generated_ipv6_cidr_block = true
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = var.subnet_cidr
 
+  tags = {
+    Name = "subnet1"
+  }
+}
+resource "aws_internet_gateway" "igw1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "igw1"
+  }
+}
+resource "aws_egress_only_internet_gateway" "egw1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "egw1"
+  }
+}
